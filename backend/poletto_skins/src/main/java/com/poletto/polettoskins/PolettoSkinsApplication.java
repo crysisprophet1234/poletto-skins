2c568b4babdc801000c39c4fccdecb10dae2f780
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-06-14 00:06:43 -0300

    Commit inicial do backend, projeto Spring Boot com repositório Maven e Java 17

---
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/PolettoSkinsApplication.java
@@ -0,0 +1,13 @@
+package com.poletto.polettoskins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolettoSkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolettoSkinsApplication.class, args);
+	}
+
+}
---
+++ backend/poletto_skins/src/test/java/com/poletto/polettoskins/PolettoSkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poletto.polettoskins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolettoSkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
