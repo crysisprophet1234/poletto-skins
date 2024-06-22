4d38cec22ef2719780b3f6a6f4570bd05a4acd35
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-06-22 16:53:07 -0300

    Setting up initial routes

--- frontend/poletto_skins/src/App.tsx
+++ frontend/poletto_skins/src/App.tsx
@@ -1,20 +1,53 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
-import Home from './pages/Home'
-
+import { lazy, Suspense } from 'react'
 import './assets/styles/global.scss'
+import ErrorPage from './pages/ErrorPage'
+import Sell from './pages/Sell'
+import Root from './routes/root'
+
+const Buy = lazy(() => import('./pages/Buy'))
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element:
+          <Suspense
+            fallback={<h1>Loading...</h1>}>
+            <Navigate to={'buy'} replace />
+          </Suspense>
+      },
+      {
+        path: '/buy',
+        element:
+          <Suspense
+            fallback={<h1>Loading...</h1>}>
+            <Buy />
+          </Suspense>
+      },
+      {
+        path: '/sell',
+        element:
+          <Suspense
+            fallback={<h1>Loading...</h1>}>
+            <Sell />
+          </Suspense>
+      }
+    ]
+  }
 ])
 
 const App = () => {
 
   return (
-    <RouterProvider router={router} />
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
---
+++ frontend/poletto_skins/src/routes/root.tsx
@@ -0,0 +1,20 @@
+import Navbar from '../components/Navbar'
+import Home from '../pages/Home'
+
+const Root = () => {
+
+    return (
+
+        <>
+
+            <Navbar />
+
+            <Home />
+
+        </>
+
+    )
+
+}
+
+export default Root
