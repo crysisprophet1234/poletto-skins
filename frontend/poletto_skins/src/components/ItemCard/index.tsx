e7358f6d1332296dfab98d5f335c0b8891e7b11d
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-06-22 16:59:15 -0300

    versão inicial do componente item card

---
+++ frontend/poletto_skins/src/components/ItemCard/index.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react'
+import './styles.css'
+
+import { ProductSkinCS } from '@/types/entities/productSkinCs'
+import { FaSteam } from "react-icons/fa6"
+import { HiShoppingCart } from 'react-icons/hi'
+import { MdFavorite } from 'react-icons/md'
+
+interface ItemCardProps {
+    itemProps: ProductSkinCS
+}
+
+//TODO: add hover effect to add-cart button
+const ItemCard = ({ itemProps }: ItemCardProps) => {
+
+    const [item, setItem] = useState<ProductSkinCS>()
+
+    useEffect(() => {
+
+        console.log('initializing itemProps')
+        setItem(itemProps)
+
+    }, [itemProps])
+
+    return (
+        <div className='item-card-main-container'>
+
+            <div className='item-info-container'>
+                <span className='skin-name'>{item?.name}</span>
+                <span className='category'>{item?.category} | {item?.weapon}</span>
+                <div className='additional-info'>
+                    {item?.statTrak &&
+                        <span className='stat-trak'>ST</span>
+                    }
+                    <span className='float-short'>{item?.floatShort}</span>
+                    <span className='float-full'>{item?.floatFull}</span>
+                </div>
+            </div>
+
+            <div className='item-picture-container'>
+
+                <div className='skin-image'>
+                    <img src={item?.itemImgUrl} alt='skin-image' />
+                </div>
+
+                <div className='sticker-stack'>
+
+                    {item?.stickerImgUrlArray.map(sticker => {
+                        return (
+                            <div className='sticker'>
+                                <img src={sticker.imgUrl} alt='sticker' key={sticker.id} />
+                            </div>
+                        )
+                    })}
+
+                </div>
+
+            </div>
+
+            <div className='item-bottom-container'>
+
+                <div className='price-container'>
+
+                    {item?.discount && item.discount > 0 &&
+                        <div className='discount'>
+                            <span>-{item.discount}%</span>
+                        </div>
+                    }
+
+                    <div className='price'>
+                        <span>R$ {item?.price}</span>
+                    </div>
+
+                    <div className='steam-price'>
+                        <FaSteam />
+                        <span>R$ {item?.steamPrice}</span>
+                    </div>
+
+                </div>
+
+                <div className='favorite-container'>
+
+                    <div className='favorite'>
+                        <MdFavorite />
+                    </div>
+
+                </div>
+
+                <div className='add-cart-container'>
+                    <div className='add-cart'>
+                        <HiShoppingCart />
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ItemCard
