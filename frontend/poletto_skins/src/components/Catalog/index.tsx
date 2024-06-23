44798d6c4b98695a36ee8d36b8447d77471ce6b6
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-06-23 15:14:33 -0300

    Adaptando a nova definição de types

--- frontend/poletto_skins/src/components/Catalog/index.tsx
+++ frontend/poletto_skins/src/components/Catalog/index.tsx
@@ -1,12 +1,39 @@
 import './styles.css'
 
-import { ProductSkinCS } from '@/types/entities/productSkinCs'
+import { Sticker, WeaponSkin } from '@/types/entities/item'
 import ItemCard from '../ItemCard'
 
 
 const Catalog = () => {
 
-    const fetchedSkinsFromApi: [ProductSkinCS] = [
+    const fetchedStickersFromApi: Sticker[] = [
+        {
+            id: 1,
+            name: 'Titan',
+            imageUrl: 'https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXQ9QVcJY8gulRYQV_bRvCiwMbQVg8kdFAYorOxKglf2_zEfnNA7oiyx9jdzqanZb3Txj8F7cZwie3CrNTwiVbl-hdrMj30dY-VewA5fxiOrQhGIm55',
+            price: 153.95,
+            steamPrice: 589.72,
+            discount: 74,
+            rarity: 'Remarkable',
+            finish: 'Holo',
+            eventOrCollection: 'Katowice 2014',
+            origin: 'Capsule'
+        },
+        {
+            id: 2,
+            name: 'Virtus.Pro',
+            imageUrl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXQ9QVcJY8gulRYX0DbRvCiwMbQVg8kdFEYoLO3PxJzw-HHTjtN5dD4ktPflvP1Z-vVkG5TsJUi2eqZp4jz2Qay_hdkMW-nINPDJFI5ZFnR_0_-n7lgPHan2Q/360fx360f',
+            price: 200.00,
+            steamPrice: 650.00,
+            discount: 69,
+            rarity: 'Remarkable',
+            finish: 'Foil',
+            eventOrCollection: 'Katowice 2015',
+            origin: 'Capsule'
+        }
+    ]
+
+    const fetchedSkinsFromApi: WeaponSkin[] = [
         {
             id: 1,
             name: 'Neon Rider',
@@ -15,17 +42,8 @@ const Catalog = () => {
             statTrak: true,
             floatShort: 'MW',
             floatFull: 0.09875,
-            itemImgUrl: 'https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJM6dO4q5KCk_LmDLfYkWNFppYojOvFpdj20QKxqEY_a2r1cIadIwdoMl3Q8lG9l7y7g5C5u5nIznJ9-n51YP1b6DQ',
-            stickerImgUrlArray: [
-                {
-                    id: 1,
-                    imgUrl: 'https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/emskatowice2014/titan_holo.bfea30263342666a115079ef5eba50949eb3c301.png',
-                },
-                {
-                    id: 2,
-                    imgUrl: 'https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/emskatowice2014/titan_holo.bfea30263342666a115079ef5eba50949eb3c301.png'
-                }
-            ],
+            imageUrl: 'https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJM6dO4q5KCk_LmDLfYkWNFppYojOvFpdj20QKxqEY_a2r1cIadIwdoMl3Q8lG9l7y7g5C5u5nIznJ9-n51YP1b6DQ',
+            stickerArray: fetchedStickersFromApi,
             discount: 22,
             price: 2389.45,
             steamPrice: 2752.73
@@ -34,7 +52,7 @@ const Catalog = () => {
 
     const renderCount = 24
 
-    const multipliedArray: ProductSkinCS[] = Array(renderCount).fill({ ...fetchedSkinsFromApi[0] })
+    const multipliedArray: WeaponSkin[] = Array(renderCount).fill({ ...fetchedSkinsFromApi[0] })
 
     return (
 
@@ -42,9 +60,9 @@ const Catalog = () => {
 
             <div className='item-container'>
 
-                {multipliedArray.map(skin => {
+                {multipliedArray.map((skin, index) => {
                     return (
-                        <ItemCard itemProps={skin} key={skin.id} />
+                        <ItemCard itemProps={skin} key={index} />
                     )
                 })
 
