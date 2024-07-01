1bb6041198a3d076e5d104ce619f0279f7e7cb11
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-06-30 21:06:32 -0300

    Usando os filtros e lógica de render do Catalog O componente Buy deve receber os valores selecionados nos filtros criados, fazer a requisição para a API com esses valores e depois renderizar o Catalog com os itens retornados, no futuro também será necessário alterar alguns comportamentos do Catalog baseado em qual componente parente está o utilizando.

--- frontend/poletto_skins/src/pages/Buy/index.tsx
+++ frontend/poletto_skins/src/pages/Buy/index.tsx
@@ -1,24 +1,73 @@
-import Catalog from '../../components/Catalog'
 import './styles.css'
 
+import { ItemType } from '@/types/entities/item'
+import axios from 'axios'
+import { useCallback, useEffect, useState } from 'react'
+import TopFilter from '../..//components/TopFilter'
+import Catalog from '../../components/Catalog'
+import MainFilter from '../../components/MainFilter'
+
+type FilterData = {
+    minPrice?: string,
+    maxPrice?: string,
+    minFloat?: string,
+    maxFloat?: string,
+    category?: string[],
+    others?: string[],
+    query?: string,
+    sort?: string,
+    favorite?: boolean
+}
+
 const Buy = () => {
+
+    const [items, setItems] = useState<ItemType[]>([])
+
+    const [filterData, setFilterData] = useState<FilterData>()
+
+    const handleFilterChange = (data: FilterData) => {
+        setFilterData(prevFilterData => ({
+            ...prevFilterData,
+            ...data
+        }))
+    }
+
+    const sendApiRequest = useCallback(async () => {
+
+        if (!filterData) return
+
+        //testing only
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            params: filterData
+        })
+
+        const data = await response.data
+        setItems(data)
+    }, [filterData])
+
+    useEffect(() => {
+        sendApiRequest()
+    }, [sendApiRequest])
 
     return (
 
         <div className='buy-main-container'>
 
             <div className='main-filter'>
-                LEFT-FILTER
+                <MainFilter onFilterChange={handleFilterChange} />
             </div>
 
             <div className='buy-main-area'>
 
                 <div className='top-filter'>
-                    TOP-FILTER
+                    <TopFilter onFilterChange={handleFilterChange} />
                 </div>
 
                 <div className='catalog-container'>
-                    <Catalog />
+                    <Catalog data={items} />
                 </div>
 
             </div>
