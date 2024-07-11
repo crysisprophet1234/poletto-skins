bffc820bd879dbfd2a65805ea46c98723ca2a204
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-07-11 00:32:41 -0300

    Corrigindo bgColor/hover:bgColor Agora o componente AddCartButton possui uma propriedade opcional isHovered para que o parente consiga informar se o seu elemento está sendo hoverado, isso é necessário pois são aplicados estilos diferentes no botão quando há hover no ItemCard em si (exemplo de parente) mas quando é no botão mesmo o estilo muda.
    backgroundColor: isItemInCart ? '#f05f75' : isHovered ? '#806cf5' : '#292733'

--- frontend/poletto_skins/src/components/AddCartButton/index.tsx
+++ frontend/poletto_skins/src/components/AddCartButton/index.tsx
@@ -3,12 +3,11 @@ import { Box, Button } from '@mui/material'
 
 type AddCartButtonProps = {
     isItemInCart: boolean
+    isHovered?: boolean
     onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
-const AddCartButton = ({ isItemInCart, onClick }: AddCartButtonProps) => {
-
-    //TODO: colors are batshit as of rn because of how the color is changed when hovering parent
+const AddCartButton = ({ isItemInCart, onClick, isHovered }: AddCartButtonProps) => {
 
     return (
 
@@ -17,15 +16,13 @@ const AddCartButton = ({ isItemInCart, onClick }: AddCartButtonProps) => {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                //width: '100%',
                 height: '100%',
                 cursor: 'pointer',
-                backgroundColor: isItemInCart ? '#f05f75' : '#292733',
+                backgroundColor: isItemInCart ? '#f05f75' : isHovered ? '#806cf5' : '#292733',
                 borderRadius: '0.25rem',
                 transition: 'background-color 0.3s ease-in-out',
                 '&:hover': {
-                    backgroundColor: isItemInCart ? '#ff8095 !important' : '#C85CD1 !important',
-                    //boxShadow: '0 0 10px rgba(200, 92, 209, 0.8), 0 0 15px rgba(200, 92, 209, 0.6), 0 0 20px rgba(200, 92, 209, 0.4)',
+                    backgroundColor: isItemInCart ? '#ff8095' : '#9F8FFF'
                 }
             }}
             onClick={onClick}
@@ -35,7 +32,6 @@ const AddCartButton = ({ isItemInCart, onClick }: AddCartButtonProps) => {
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
-                    //width: '100%',
                     height: '100%',
                     minWidth: 0,
                     padding: '6px',
