d9a850176b3af34a26ca3198433d3c2734378fe3
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-07-14 22:07:06 -0300

    Métodos de requisição para API com axios
    Get, Post, Put e Del, sempre irão receber o path e opcionalmente podem
    receber query params e payload.

---
+++ frontend/poletto_skins/src/services/api.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosResponse } from 'axios'
+
+const apiUrl = import.meta.env.VITE_POLETTO_SKINS_API_URL
+const apiPort = import.meta.env.VITE_POLETTO_SKINS_API_PORT
+const apiBaseUrl = apiUrl + ':' + apiPort
+
+const api = axios.create({
+    baseURL: apiBaseUrl,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+type Params = Record<string, unknown>
+type Data = Record<string, unknown>
+
+export const get = async <T>(url: string, params?: Params): Promise<T> => {
+
+    const response: AxiosResponse<T> = await api.get(url, { params })
+    return response.data
+}
+
+export const post = async <T>(url: string, data?: Data): Promise<T> => {
+
+    const response: AxiosResponse<T> = await api.post(url, data)
+    return response.data
+}
+
+export const put = async <T>(url: string, data?: Data): Promise<T> => {
+
+    const response: AxiosResponse<T> = await api.put(url, data)
+    return response.data
+}
+
+export const del = async <T>(url: string): Promise<T> => {
+
+    const response: AxiosResponse<T> = await api.delete(url)
+    return response.data
+}
