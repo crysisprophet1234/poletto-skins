f3af9aecce0995f3b532b51d4a471df1dfa4b587
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-07-21 02:41:28 -0300

    Adicionado método getUserInventory O novo método getUserInventory busca os itens no invetário do usuário pelo steamId providenciado, fazendo primeiro uma busca na api da steam e depois usando o serviço da CsFloat para tratar os dados dos itens para um formado mais próximo da necessidade do client.

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/SteamService.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/SteamService.java
@@ -1,11 +1,17 @@
 package com.poletto.polettoskins.services;
 
+import java.util.List;
+
 import org.springframework.transaction.annotation.Transactional;
 
+import com.poletto.polettoskins.entities.SteamItem;
 import com.poletto.polettoskins.entities.SteamUser;
 
 public interface SteamService {
 
 	@Transactional(readOnly = true)
-	SteamUser getPlayerInfo(String steamId);
+	SteamUser getUserInfo(String steamId);
+	
+	@Transactional(readOnly = true)
+	List<SteamItem> getUserInventory(String steamId);
 }
