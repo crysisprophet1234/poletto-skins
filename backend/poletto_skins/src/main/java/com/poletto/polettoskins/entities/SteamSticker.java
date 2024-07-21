856edf85869ddbf9a09c2365ce494ec6b04eb337
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-07-21 02:39:34 -0300

    Entidades SteamItem e SteamSticker Essas entidades representam os itens do usuário obtidos pela api da Steam e tratados pela api da CSFloat e são identificados pela assetId e ownerId.

---
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/entities/SteamSticker.java
@@ -0,0 +1,134 @@
+package com.poletto.polettoskins.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "steam_sticker")
+public class SteamSticker implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Integer stickerId;
+	private Integer slot;
+    private Double wear;
+    private Double scale;
+    private Integer rotation;
+    private String codename;
+    private String material;
+    private String name;
+    
+    public SteamSticker() {}
+    
+	public SteamSticker(
+		Integer stickerId,
+		Integer slot,
+		Double wear,
+		Double scale,
+		Integer rotation,
+		String codename,
+		String material,
+		String name
+	) {
+		this.stickerId = stickerId;
+		this.slot = slot;
+		this.wear = wear;
+		this.scale = scale;
+		this.rotation = rotation;
+		this.codename = codename;
+		this.material = material;
+		this.name = name;
+	}
+
+	public Integer getStickerId() {
+		return stickerId;
+	}
+
+	public void setStickerId(Integer stickerId) {
+		this.stickerId = stickerId;
+	}
+
+	public Integer getSlot() {
+		return slot;
+	}
+
+	public void setSlot(Integer slot) {
+		this.slot = slot;
+	}
+
+	public Double getWear() {
+		return wear;
+	}
+
+	public void setWear(Double wear) {
+		this.wear = wear;
+	}
+
+	public Double getScale() {
+		return scale;
+	}
+
+	public void setScale(Double scale) {
+		this.scale = scale;
+	}
+
+	public Integer getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(Integer rotation) {
+		this.rotation = rotation;
+	}
+
+	public String getCodename() {
+		return codename;
+	}
+
+	public void setCodename(String codename) {
+		this.codename = codename;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(stickerId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SteamSticker other = (SteamSticker) obj;
+		return stickerId == other.stickerId;
+	}
+
+	@Override
+	public String toString() {
+		return "SteamSticker [stickerId=" + stickerId + ", slot=" + slot + ", wear=" + wear + ", scale=" + scale
+				+ ", rotation=" + rotation + ", codename=" + codename + ", material=" + material + ", name=" + name
+				+ "]";
+	}
+
+}
