24dd1b8365952bd399b2cd69c5886a3d3f5f7472
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-11 20:55:12 -0300

    adapting to new dto classes naming

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/DomainUserService.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/DomainUserService.java
@@ -3,22 +3,22 @@ package com.poletto.polettoskins.services;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.poletto.polettoskins.dto.DomainUserDto;
+import com.poletto.polettoskins.dto.DomainUserDTO;
 import com.poletto.polettoskins.entities.SteamUser;
 
 @Service
 public interface DomainUserService {
 	
 	@Transactional(readOnly = true)
-	DomainUserDto findUserById(String userId);
+	DomainUserDTO findUserById(String userId);
 	
 	@Transactional(readOnly = true)
-	DomainUserDto findOrRegisterUserBySteamId(String steamId);
+	DomainUserDTO findOrRegisterUserBySteamId(String steamId);
 	
 	@Transactional
-	DomainUserDto createUser(SteamUser steamUser);
+	DomainUserDTO createUser(SteamUser steamUser);
 	
 	@Transactional
-	DomainUserDto updateEmail(String userId, DomainUserDto domainUserDto);
+	DomainUserDTO updateEmail(String userId, DomainUserDTO domainUserDto);
 
 }
