1dc600dd6602e8a73e47a320392a61335d5beaef
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-17 23:11:16 -0300

    adjusting naming convention for marketitem

--- frontend/poletto_skins/src/utils/extractItemId.ts
+++ frontend/poletto_skins/src/utils/extractItemId.ts
@@ -1,6 +1,6 @@
 import { MarketItem } from '@/types/entities/steam-item'
 
-export const extractItemId = (item: MarketItem) => {
-    const url = item.inspectUrl
+export const extractItemId = (marketItem: MarketItem) => {
+    const url = marketItem.item.inspectUrl
     return url?.split('%20')[1] || ''
 }
