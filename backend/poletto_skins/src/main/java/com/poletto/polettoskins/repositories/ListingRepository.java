98f22abc4b242885b33106f4cfff1055124e08b0
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-17 23:06:27 -0300

    query para descobrir itens anunciados por id

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/repositories/ListingRepository.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/repositories/ListingRepository.java
@@ -1,5 +1,7 @@
 package com.poletto.polettoskins.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,39 +15,43 @@ public interface ListingRepository extends MongoRepository<Listing, String> {
 	
 	//TODO: listing value should be stored as a double anyway
 	@Query("{ $and: [ " +
-		       "  { $or: [ " +
-		       "    { $expr: { $eq: ['', ?0] } }, " +
-		       "    { 'item.fullItemName': { $regex: ?0, $options: 'i' } }" +
-		       "  ] }, " +
-		       "  { $or: [ " +
-		       "    { $expr: { $eq: ['', ?1] } }, " +
-		       "    { userId: ?1 }" +
-		       "  ] }, " +
-		       "  { $or: [ " +
-		       "    { $expr: { $eq: [null, ?2] } }, " +
-		       "    { $expr: { $gte: [ { $toDouble: '$listingPrice' }, ?2 ] } }" +
-		       "  ] }, " +
-		       "  { $or: [ " +
-		       "    { $expr: { $eq: [null, ?3] } }, " +
-		       "    { $expr: { $lte: [ { $toDouble: '$listingPrice' }, ?3 ] } }" +
-		       "  ] }, " +
-		       "  { $or: [ " +
-		       "    { $expr: { $eq: [null, ?4] } }, " +
-		       "    { 'item.floatValue': { $gte: ?4 } }" +
-		       "  ] }, " +
-		       "  { $or: [ " +
-		       "    { $expr: { $eq: [null, ?5] } }, " +
-		       "    { 'item.floatValue': { $lte: ?5 } }" +
-		       "  ] }" +
-		       "] }")
-		Page<Listing> findListingsPaged(
-		    Pageable pageable,
-		    String query,
-		    String userId,
-		    Double minPrice,
-		    Double maxPrice,
-		    Double minFloat,
-		    Double maxFloat
-		);
+	       "  { $or: [ " +
+	       "    { $expr: { $eq: ['', ?0] } }, " +
+	       "    { 'item.fullItemName': { $regex: ?0, $options: 'i' } }" +
+	       "  ] }, " +
+	       "  { $or: [ " +
+	       "    { $expr: { $eq: ['', ?1] } }, " +
+	       "    { userId: ?1 }" +
+	       "  ] }, " +
+	       "  { $or: [ " +
+	       "    { $expr: { $eq: [null, ?2] } }, " +
+	       "    { $expr: { $gte: [ { $toDouble: '$listingPrice' }, ?2 ] } }" +
+	       "  ] }, " +
+	       "  { $or: [ " +
+	       "    { $expr: { $eq: [null, ?3] } }, " +
+	       "    { $expr: { $lte: [ { $toDouble: '$listingPrice' }, ?3 ] } }" +
+	       "  ] }, " +
+	       "  { $or: [ " +
+	       "    { $expr: { $eq: [null, ?4] } }, " +
+	       "    { 'item.floatValue': { $gte: ?4 } }" +
+	       "  ] }, " +
+	       "  { $or: [ " +
+	       "    { $expr: { $eq: [null, ?5] } }, " +
+	       "    { 'item.floatValue': { $lte: ?5 } }" +
+	       "  ] }" +
+	       "] }"
+	)
+	Page<Listing> findListingsPaged(
+	    Pageable pageable,
+	    String query,
+	    String userId,
+	    Double minPrice,
+	    Double maxPrice,
+	    Double minFloat,
+	    Double maxFloat
+	);
+	
+	@Query("{ 'item.assetId': { $in: ?0 } }")
+	List<Listing> findAllByAssetIdIn(List<String> assetIds);
 
 }
