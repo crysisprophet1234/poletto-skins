f5a8ebfd6d941c00699227c4131ac585340608a2
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-28 01:32:29 -0300

    fetch single item endpoint removed

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/controllers/ItemController.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/controllers/ItemController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.poletto.polettoskins.entities.SteamItem;
 import com.poletto.polettoskins.entities.SteamItemPrice;
-import com.poletto.polettoskins.exceptions.response.ResourceNotFoundException;
 import com.poletto.polettoskins.services.impl.SteamServiceImpl;
 
 @RestController
@@ -17,12 +15,6 @@ public class ItemController {
 	
 	@Autowired
 	private SteamServiceImpl steamService;
-	
-	@GetMapping("/{itemSteamId}")
-	public SteamItem getItemBySteamId(@PathVariable String itemSteamId) {
-		return steamService.getItemBySteamId(itemSteamId)
-				.orElseThrow(() -> new ResourceNotFoundException("Invalid item or item name not found."));
-	}
 	
 	@Deprecated
 	@GetMapping("/{itemSteamId}/market-price")
