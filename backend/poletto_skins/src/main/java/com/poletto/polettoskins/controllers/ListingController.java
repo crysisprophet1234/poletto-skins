abaf28eba9f10383f9d5d11a5d82bda397589695
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-28 02:06:26 -0300

    endpoint /listings now receives status query param

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/controllers/ListingController.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/controllers/ListingController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poletto.polettoskins.dto.ListingDTO;
+import com.poletto.polettoskins.entities.enums.ListingStatus;
 import com.poletto.polettoskins.services.ListingService;
 
 @RestController
@@ -28,13 +29,14 @@ public class ListingController {
 	public Page<ListingDTO> getListingsPaged(
 	    @RequestParam(required = false, defaultValue = "") String query,
 	    @RequestParam(required = false, defaultValue = "") String userId,
+	    @RequestParam(required = false) ListingStatus status,
 	    @RequestParam(required = false) Double minPrice,
 	    @RequestParam(required = false) Double maxPrice,
 	    @RequestParam(required = false) Double minFloat,
 	    @RequestParam(required = false) Double maxFloat,
 	    @PageableDefault(page = 0, size = 12, sort = "createdOn", direction = Direction.DESC) Pageable pageable
 	) {
-	    return listingService.findListingsPaged(pageable, query, userId, minPrice, maxPrice, minFloat, maxFloat);
+	    return listingService.findListingsPaged(pageable, query, userId, status, minPrice, maxPrice, minFloat, maxFloat);
 	}
 	
 	@PostMapping
