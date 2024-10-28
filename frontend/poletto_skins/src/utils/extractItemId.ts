93aa271b9ce518af6b2491a077e693aa14f2fa39
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-28 01:36:52 -0300

    adapting to new marketItem definition

--- frontend/poletto_skins/src/utils/extractItemId.ts
+++ frontend/poletto_skins/src/utils/extractItemId.ts
@@ -1,6 +1,6 @@
 import { MarketItem } from '@/types/entities/steam-item'
 
 export const extractItemId = (marketItem: MarketItem) => {
-    const url = marketItem.item.inspectUrl
+    const url = marketItem.inspectUrl
     return url?.split('%20')[1] || ''
 }
