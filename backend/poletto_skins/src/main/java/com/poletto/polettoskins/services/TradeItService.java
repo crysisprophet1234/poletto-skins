440df3ed24b45d525df0f91f6ddb75cc21210670
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-10-29 21:02:32 -0300

    now returns a list of marketitems
    also added new method to convert usd into brl

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/TradeItService.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/TradeItService.java
@@ -1,16 +1,20 @@
 package com.poletto.polettoskins.services;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.poletto.polettoskins.entities.SteamItem;
+import com.poletto.polettoskins.entities.MarketItem;
 
 @Service
 public interface TradeItService {
 	
 	@Transactional(readOnly = true)
-	List<SteamItem> fetchInventoryBySteamId(String steamId);
+	List<MarketItem> fetchInventoryBySteamId(String steamId);
+	
+	@Transactional(readOnly = true)
+	BigDecimal convertUsdToBrl(BigDecimal usd);
 
 }
