5fec5eeebe0a40e76a8c0460ea5340636df99f32
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-11-02 01:32:44 -0300

    fixing import port variable

--- frontend/poletto_skins/src/services/api.ts
+++ frontend/poletto_skins/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from 'axios'
 
 const apiUrl = import.meta.env.VITE_POLETTO_SKINS_API_URL || process.env.POLETTO_SKINS_API_URL
-const apiPort = import.meta.env.VITE_POLETTO_SKINS_API_PORT || process.env.POLETTO_SKINS_API_URL
-const apiBaseUrl = apiUrl + ':' + apiPort + '/api'
+const apiPort = import.meta.env.VITE_POLETTO_SKINS_API_PORT || process.env.POLETTO_SKINS_API_PORT
+const apiBaseUrl = `${apiUrl}:${apiPort}/api`
 
 const api = axios.create({
     baseURL: apiBaseUrl,
