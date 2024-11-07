2b0ed29a3e29d346f2d0edddd96820dc005162f3
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-11-07 19:08:34 -0300

    util for building requests

---
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/entities/utils/HttpRequestBuilderUtil.java
@@ -0,0 +1,21 @@
+package com.poletto.polettoskins.entities.utils;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class HttpRequestBuilderUtil {
+
+	public static UriComponentsBuilder buildUri(String baseUrl, String... pathSegments) {
+        return UriComponentsBuilder.fromHttpUrl(baseUrl).pathSegment(pathSegments);
+    }
+	
+	public static HttpRequest buildRequest(String uri) {
+		return HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .GET()
+                .build();
+	}
+	
+}
