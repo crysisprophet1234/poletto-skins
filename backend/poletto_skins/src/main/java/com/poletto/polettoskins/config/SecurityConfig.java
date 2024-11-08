3a1acd0f6d618bacfcfeff06833583a450951e52
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-11-08 17:20:46 -0300

    using jwt filter and custom 401 and 403 responses
    allowing login methods and get listings

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/config/SecurityConfig.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.poletto.polettoskins.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,12 +23,33 @@ public class SecurityConfig {
 	@Value("${cors.allowed-origin}")
 	private String corsAllowedOrigin;
 	
+	@Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	@Autowired
+	private CustomAccessDeniedHandler customAccessDeniedHandler;
+	
+	@Autowired
+	private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http
 			.csrf(csrf -> csrf.disable())
 			//.requiresChannel(channel -> channel.anyRequest().requiresSecure())
+
+			.authorizeHttpRequests(authz -> authz
+	            .requestMatchers(HttpMethod.GET, "/login", "/openidreturn", "/listings").permitAll()
+	            .anyRequest().authenticated()
+	        )
+			
+			.exceptionHandling(exception -> exception
+                .accessDeniedHandler(customAccessDeniedHandler)
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+            )
+			
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 			.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 		
