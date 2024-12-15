eea72d36b74c8caec4b8a0b7c1bc9c971b94c289
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-12-15 02:11:35 -0300

    adding sort parameter to find inventory

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/InventoryService.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/InventoryService.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.poletto.polettoskins.dto.InventoryDTO;
+import com.poletto.polettoskins.entities.enums.SteamWebInventorySortType;
 
 @Service
 public interface InventoryService {
 	
 	@Transactional(readOnly = true)
-	InventoryDTO getUserInventory(String steamId, int size, int page, boolean filterListed);
+	InventoryDTO getUserInventory(
+		String steamId,
+		int size,
+		int page,
+		SteamWebInventorySortType sort,
+		boolean filterListed
+	);
 
 }
