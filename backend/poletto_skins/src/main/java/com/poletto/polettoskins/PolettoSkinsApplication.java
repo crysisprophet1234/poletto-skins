8c25fa1f8b319598ff5a87bd358fd38c9ccf841c
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-12-15 02:12:13 -0300

    enabling retry policy and caching

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/PolettoSkinsApplication.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/PolettoSkinsApplication.java
@@ -2,8 +2,12 @@ package com.poletto.polettoskins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
+@EnableCaching
 public class PolettoSkinsApplication {
 
 	public static void main(String[] args) {
