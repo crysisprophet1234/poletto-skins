66850e9c6b19565fa705077466b4005765070d2e
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-12-15 02:11:48 -0300

    new find user inventory method

--- backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/SteamService.java
+++ backend/poletto_skins/src/main/java/com/poletto/polettoskins/services/SteamService.java
@@ -1,8 +1,11 @@
 package com.poletto.polettoskins.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.poletto.polettoskins.entities.MarketItem;
 import com.poletto.polettoskins.entities.SteamItemPrice;
 import com.poletto.polettoskins.entities.SteamUser;
 
@@ -11,6 +14,9 @@ public interface SteamService {
 
 	@Transactional(readOnly = true)
 	SteamUser findSteamUserBySteamId(String steamId);
+	
+	@Transactional(readOnly = true)
+	List<MarketItem> findUserInventoryBySteamId(String steamId);
 	
 	@Transactional(readOnly = true)
 	SteamItemPrice findSteamItemPriceByName(String fullItemName);
